91dbedc8a47925a6ac51cc1212e52197fc3c92aa
shining013 <nevertheless113@naver.com>
2024-04-23 22:59:22 +0900

    feat: 추상화에 의존하는 코드

--- Downloads/product.management/src/main/java/kr/co/yunju/product/management/application/SimpleProductService.java
+++ Downloads/product.management/src/main/java/kr/co/yunju/product/management/application/SimpleProductService.java
@@ -2,7 +2,7 @@ package kr.co.yunju.product.management.application;
 
 import kr.co.yunju.product.management.domain.Product;
 import kr.co.yunju.product.management.infrastructure.DatabaseProductRepository;
-import kr.co.yunju.product.management.infrastructure.ListProductRepository;
+import kr.co.yunju.product.management.domain.ProductRepository;
 import kr.co.yunju.product.management.presentation.ProductDto;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +12,12 @@ import java.util.List;
 
 @Service
 public class SimpleProductService {
-    private DatabaseProductRepository databaseProductRepository;
+    private ProductRepository productRepository;
     private ModelMapper modelMapper;
     private ValidationService validationService;
     @Autowired
-    SimpleProductService(DatabaseProductRepository databaseProductRepository, ModelMapper modelMapper, ValidationService validationService) {
-        this.databaseProductRepository = databaseProductRepository;
+    SimpleProductService(ProductRepository productRepository, ModelMapper modelMapper, ValidationService validationService) {
+        this.productRepository = productRepository;
         this.modelMapper = modelMapper;
         this.validationService = validationService;
     }
@@ -26,19 +26,19 @@ public class SimpleProductService {
         Product product = modelMapper.map(productDto, Product.class);
         validationService.checkValid(product);
 
-        Product savedProduct = databaseProductRepository.add(product);
+        Product savedProduct = productRepository.add(product);
         ProductDto savedProductDto = modelMapper.map(savedProduct, ProductDto.class);
         return savedProductDto;
     }
 
     public ProductDto findById(Long id) {
-        Product product = databaseProductRepository.findById(id);
+        Product product = productRepository.findById(id);
         ProductDto productDto = modelMapper.map(product, ProductDto.class);
         return productDto;
     }
 
     public List<ProductDto> findAll() {
-        List<Product> products = databaseProductRepository.findAll();
+        List<Product> products = productRepository.findAll();
         List<ProductDto> productDtos = products.stream()
                 .map(product -> modelMapper.map(product, ProductDto.class))
                 .toList();
@@ -46,7 +46,7 @@ public class SimpleProductService {
     }
 
     public List<ProductDto> findByNameContaining(String name) {
-        List<Product> products = databaseProductRepository.findByNameContaining(name);
+        List<Product> products = productRepository.findByNameContaining(name);
         List<ProductDto> productDtos = products.stream()
                 .map(product -> modelMapper.map(product, ProductDto.class))
                 .toList();
@@ -55,12 +55,12 @@ public class SimpleProductService {
 
     public ProductDto update(ProductDto productDto) {
         Product product = modelMapper.map(productDto, Product.class);
-        Product updateProduct = databaseProductRepository.update(product);
+        Product updateProduct = productRepository.update(product);
         ProductDto updatedProductDto = modelMapper.map(updateProduct, ProductDto.class);
         return updatedProductDto;
     }
 
     public void delete(Long id) {
-        databaseProductRepository.delete(id);
+        productRepository.delete(id);
     }
 }
